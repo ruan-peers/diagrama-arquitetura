6e793151332c00b4f4f2e7542d3a6d7bd5464be5
ruan-peers <ruan.neves@peers.com.br>
2026-04-02 08:06:31 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANTE: O 'base' deve ser o nome do seu repositório no GitHub entre barras.
+  // Baseado no seu link anterior (ruan-peers.github.io/diagrama-arquitetura/), o valor deve ser:
+  base: '/diagrama-arquitetura/',
 })
